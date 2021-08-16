c1f072c00c3545ae07a56aeb33bba1cfd2eab301
saurabh0411 <saurabh_kumar@lilly.com>
2021-08-16 14:57:13 +0000

    Project setup file

---
+++ prj_setup.r
@@ -0,0 +1,16 @@
+library(usethis)
+
+usethis::create_github_token()
+
+credentials::set_github_pat()
+
+use_git_config(user.name = "saurabh0411", user.email = "saurabh_kumar@lilly.com")
+
+
+usethis::edit_r_environ()
+
+
+
+Sys.getenv("GITHUB_PAT_lilly")
+
+credentials::set_github_pat(TRUE)
