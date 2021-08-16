67872bd372c9b81e7342982026e8a1da35ca22e3
saurabh0411 <saurabh_kumar@lilly.com>
2021-08-16 14:58:41 +0000

    New Comment

--- prj_setup.r
+++ prj_setup.r
@@ -2,9 +2,10 @@ library(usethis)
 
 usethis::create_github_token()
 
-credentials::set_github_pat()
+#Comment as it is not regular usage command
+#credentials::set_github_pat()
 
-use_git_config(user.name = "saurabh0411", user.email = "saurabh_kumar@lilly.com")
+#use_git_config(user.name = "saurabh0411", user.email = "saurabh_kumar@lilly.com")
 
 
 usethis::edit_r_environ()
